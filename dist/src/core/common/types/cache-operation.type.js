75c5802bc43b01f24af2214b77517f67d84e786f
Allan Vieira <allancnfx.vieira@gmail.com>
2024-03-15 18:56:26 -0300

    feat: change build and publish step

--- dist/src/core/common/types/cache-operation.type.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=cache-operation.type.js.map
--- dist/src/database/adapter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.adapter = void 0;
-const implementations_1 = require("./implementations");
-exports.adapter = {
-    ioredis: () => implementations_1.IORedisCacheRepository
-};
-//# sourceMappingURL=adapter.js.map
