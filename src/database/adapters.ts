4eb38e2dde9cdb2910afb35470d93ec97540e8dd
Allan Vieira <allancnfx.vieira@gmail.com>
2024-03-17 14:26:00 -0300

    fix: remove unused codes

--- src/database/adapters.ts
+++ src/database/adapters.ts
@@ -1,19 +1,6 @@
 import { AdaptersEnum } from "./enums";
 import { IORedisCacheRepository } from "./implementations";
 import { ICacheRepository } from "./interfaces";
-import { DataSourceOptions } from "./types";
-
-const Adapters = {
-  [AdaptersEnum.REDIS]: IORedisCacheRepository,
-  [AdaptersEnum.MONGODB]: IORedisCacheRepository
-}
-
-// const items: [AdaptersEnum, new (...args: any[]) => ICacheRepository<any, any>][] = Object.entries(Adapters).map(([key, value]) => {
-//   return [AdaptersEnum[key as keyof typeof AdaptersEnum], value];
-// });
-
-// Assuming IORedisCacheRepository is correctly typed as a constructor
-// that matches the expected signature for ICacheRepository<Adapter, any>
 
 export const AdapterMap = <Adapter extends AdaptersEnum = any, Client = any>() => {
   return new Map<AdaptersEnum, new (...args: any[]) => ICacheRepository<Adapter, Client>>([
--- src/database/types/mongodb-adapter-options.type.ts
+++ src/database/types/mongodb-adapter-options.type.ts
@@ -1,4 +1,4 @@
 export abstract class MongoDBAdapterOptions {
-  mongoHost!: string;
+  host!: string;
   port?: number
 }
