c74a76453bbe473abda28dd26f77ace85f322897
Allan Vieira <allancnfx.vieira@gmail.com>
2024-06-26 02:02:23 -0300

    fix: add access client instance in interface

--- src/database/interfaces/resource.interface.ts
+++ src/database/interfaces/resource.interface.ts
@@ -3,6 +3,7 @@ import { DataSourceOptions } from "@database/types";
 
 export abstract class IResource<Adapter extends AdaptersEnum = any, Client = any> {
   public _client?: Client
+  public client?: Client
   public abstract connect(config: DataSourceOptions<Adapter>): Promise<this>;
   public abstract disconnect(): void;
   public abstract logger(error: any): void;
